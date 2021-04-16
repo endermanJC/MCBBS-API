5ca7374a43ecc9bba2ac8011389d3a77eb2604b8
endermanJC <82296321+endermanJC@users.noreply.github.com>
2021-04-16 19:14:50 +0800

    Update project.py
    0.0.2

--- project.py
+++ project.py
@@ -2,9 +2,14 @@ import urllib.request
 import re
 
 
-class MCBBSAPI:
-    @staticmethod
-    def get(fid):
+def get(fid, mode):
+    if int(mode) == 0:
+        s = urllib.request.urlopen('https://www.mcbbs.net/forum-' + str(fid) + '-1.html?forumdefstyle=yes')
+        information2 = s.read().decode('utf-8')
+        information = re.findall('<a href="thread-(.*?)-1-1.html" (?:.*?)onclick="atarget\\(this\\)" class="s xst">'
+                                 '(.*?)</a>', information2)
+        return information
+    elif int(mode) == 1:
         s = urllib.request.urlopen('https://www.mcbbs.net/forum.php?mod=forumdisplay&fid=' + str(fid)
                                    + '&filter=author&orderby=dateline&forumdefstyle=yes')
         information2 = s.read().decode('utf-8')
